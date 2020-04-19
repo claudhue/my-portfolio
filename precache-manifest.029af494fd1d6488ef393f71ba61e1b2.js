6e490eba320c712bf05a03782ddb9f678f801532
Francisco Goya <degoyafran@gmail.com>
2020-04-19 11:57:45 -0500

    Updates

---
+++ precache-manifest.029af494fd1d6488ef393f71ba61e1b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf1d4a56b1887ff05dcebcab3d250d6",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "39e2537508f75719ba23",
+    "url": "/my-portfolio/static/css/main.b1b3e7fa.chunk.css"
+  },
+  {
+    "revision": "722678521ff8b82442eb",
+    "url": "/my-portfolio/static/js/2.e1c1c2fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.e1c1c2fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39e2537508f75719ba23",
+    "url": "/my-portfolio/static/js/main.6ed1f2a0.chunk.js"
+  },
+  {
+    "revision": "c5d14653e6c493292441",
+    "url": "/my-portfolio/static/js/runtime-main.f13b90e6.js"
+  },
+  {
+    "revision": "f00291ae9fa105eab4370718566b1bb1",
+    "url": "/my-portfolio/static/media/budget.f00291ae.png"
+  },
+  {
+    "revision": "ac760d18a8121b7e9cefed21f57054e4",
+    "url": "/my-portfolio/static/media/codeQuiz.ac760d18.png"
+  },
+  {
+    "revision": "ccf151b1d58402db12eaf2fb2dccf599",
+    "url": "/my-portfolio/static/media/mountain.ccf151b1.jpeg"
+  },
+  {
+    "revision": "15aee320d2245eec6c7472d28ce62a61",
+    "url": "/my-portfolio/static/media/passwordG.15aee320.png"
+  },
+  {
+    "revision": "241998331be8e4869deae8476c5fff99",
+    "url": "/my-portfolio/static/media/profile.24199833.png"
+  }
+]);
