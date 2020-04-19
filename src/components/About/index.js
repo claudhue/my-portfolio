2834d79fd00f6b297b5bdc31b5e97c18b408d1f3
Francisco Goya <degoyafran@gmail.com>
2020-04-19 18:47:57 -0500

    minor edits to about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,7 +11,7 @@ export default function About() {
                         <img src= {Logo} alt="profile" height="300" width="300"/>
                     </div>
                     <div class="col-md-8 text-center">
-                        <p>I recently obtained my certificate in Full Stack Web Development at Southern Methodist University.  Skills in HTML, CSS, JavaScript, jQuery, Node.js, Express.js, React.js, MongoDB, MySQL, Git, and more.  Bi-lingual professional with over 20 years of success delivering outstanding customer service in multiple areas such as technical support, retail, and medical customer service.  Excellent customer service skills concentrated on providing a professional, informed and customized experience for the client base.  Utilizing technically-sound decision making skills to ensure appropriate resolutions. Proven ability to lead and motivate a team to obtain a common project or goal.</p>
+                        <p>I recently obtained my certificate in Full Stack Web Development at Southern Methodist University.  Skills in HTML, CSS, JavaScript, jQuery, Node.js, Express.js, React.js, MongoDB, MySQL, Git, and more.  Bi-lingual professional with over 20 years of success delivering outstanding customer service in multiple areas such as technical support, finance, retail, and medical customer service.  Excellent customer service skills concentrated on providing a professional, informed, and customized experience for the client base.  Utilizing technically-sound decision making skills to ensure appropriate resolutions. Proven ability to lead and motivate a team to obtain a common project or goal.</p>
                     </div>
                 </div>
             </div>
